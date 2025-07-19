040f08a00dda0968667fd6410bb237e0212339cc
whizikem <wisdomikem@gmail.com>
2025-07-19 11:41:45 +0100

    Basic Routing

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,12 @@
+/**
+ * Landing page for the application.
+ */
+
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-extralight">Landing Page</h1>
+    </div>
+  )
+}
+export default Landing;
